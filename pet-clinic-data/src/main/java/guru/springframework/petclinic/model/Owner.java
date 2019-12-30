9e418f3ce93554602b303f7db7e137fb4e536859
Steven Henke <shenke13@gmail.com>
2019-12-30 11:38:20 -0600

    adds properties to owner entity.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -4,7 +4,40 @@ import java.util.Set;
 
 public class Owner extends Person
 {
+    private String Address;
+    private String City;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress()
+    {
+        return Address;
+    }
+
+    public void setAddress(String address)
+    {
+        Address = address;
+    }
+
+    public String getCity()
+    {
+        return City;
+    }
+
+    public void setCity(String city)
+    {
+        City = city;
+    }
+
+    public String getTelephone()
+    {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone)
+    {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets()
     {
