be586f3783a8995b2361c3c86a6d4f4d184efdcb
Steven Henke <steven@foundrymakes.com>
2020-01-03 09:50:28 -0600

    creates jpa service for Specialties.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/SpecialtySDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -7,48 +7,49 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.petclinic.model.Specialty;
+import guru.springframework.petclinic.repositories.SpecialtyRepository;
 import guru.springframework.petclinic.services.SpecialtyService;
 
 @Service
 @Profile("springdatajpa")
 public class SpecialtySDJpaService implements SpecialtyService
 {
-    private final SpecialtyService specialtyService;
+    private final SpecialtyRepository specialtyRepository;
 
-    public SpecialtySDJpaService(SpecialtyService specialtyService)
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository)
     {
-        this.specialtyService = specialtyService;
+        this.specialtyRepository = specialtyRepository;
     }
 
     @Override
     public Set<Specialty> findAll()
     {
         Set<Specialty> specialties = new HashSet<>();
-        specialtyService.findAll().forEach(specialties::add);
+        specialtyRepository.findAll().forEach(specialties::add);
         return specialties;
     }
 
     @Override
     public Specialty findById(Long id)
     {
-        return specialtyService.findById(id);
+        return specialtyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Specialty save(Specialty object)
     {
-        return specialtyService.save(object);
+        return specialtyRepository.save(object);
     }
 
     @Override
     public void delete(Specialty object)
     {
-        specialtyService.delete(object);
+        specialtyRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long id)
     {
-        specialtyService.deleteById(id);
+        specialtyRepository.deleteById(id);
     }
 }
