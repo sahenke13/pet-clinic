a55e02a9b9dcc5e728cc84e407e3587c352eec52
Steven Henke <steven@foundrymakes.com>
2020-01-08 13:45:57 -0600

    upgrade to junit 5

---
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PetClinicApplicationTest
+{
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
